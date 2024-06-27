b962a5d3f1cdd7d76b5321aef6a00410f792919d
Adam Mwaniki <adamndegwa@protonmail.com>
2024-06-27 14:08:48 +0300

    Makes some notes on routes

--- server/app.py
+++ server/app.py
@@ -3,3 +3,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>') # writing it this way ensures that it only takes in strings as the parameter. we can make it a valid int or path or float if we want
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
