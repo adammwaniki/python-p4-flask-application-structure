92e24c6910b32b7a26f5812c423189d9bd9ba783
Adam Mwaniki <adamndegwa@protonmail.com>
2024-06-27 22:08:18 +0300

    Makes a simple flsk app

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-
+'''import re
+'''
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,3 +12,9 @@ def index():
 @app.route('/<string:username>') # writing it this way ensures that it only takes in strings as the parameter. we can make it a valid int or path or float if we want
 def user(username):
     return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
+'''exp = re.compile('[A-z]+')
+type, parameter = exp.findall(url)'''
